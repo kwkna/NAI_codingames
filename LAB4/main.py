4021bc98614053caee6060e2c9089aa60220e1b5
kwkna <kwkoscierzyna@outlook.com>
2020-12-19 11:45:25 +0100

    add 6 not recommended movies function in moviesRecomender.py

--- LAB4/main.py
+++ LAB4/main.py
@@ -7,7 +7,7 @@ from LAB4.moviesRecomender import MovieRecommender
 
 mr = MovieRecommender()
 mr.clean_data()
-
 print('6 recommended movies:', mr.get_6_recommended_movies('user1'))
+print('6 not recommended movies', mr.get_6_not_recommended_movies('user1'))
 # print(mr.get_recommended_movies('user1'))
 
